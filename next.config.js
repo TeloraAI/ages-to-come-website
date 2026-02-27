3d382fd2c778394e3c6ff5625b037b45cefc4571
TeloraAI <SEATTLE@BDSOPS.COM>
2026-02-26 18:31:11 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['gray-elephant-127951.hostingersite.com'],
-  },
-}
-
+const nextConfig = {}
 module.exports = nextConfig
